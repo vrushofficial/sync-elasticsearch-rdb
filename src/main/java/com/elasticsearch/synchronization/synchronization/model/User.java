bcb113b8d841b34b57c6c2d68c1b210ba34dd900
vojoshi <vrushabha.joshi@cygnetinfotech.com>
2020-12-05 14:51:43 +0530

    model

---
+++ src/main/java/com/elasticsearch/synchronization/synchronization/model/User.java
@@ -0,0 +1,60 @@
+package com.elasticsearch.synchronization.synchronization.model;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    private Date modificationDate;
+
+    public User() {
+        // do nothing
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firsName) {
+        this.firstName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
---
+++ src/main/java/com/elasticsearch/synchronization/synchronization/model/UserModel.java
@@ -0,0 +1,49 @@
+package com.elasticsearch.synchronization.synchronization.model;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.util.Date;
+
+@Document(indexName = "user")
+public class UserModel {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date modificationDate;
+
+    public UserModel() {
+        // do nothing
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
