801bdc5992d296bab91a931e1b984dd7a014110f
vojoshi <vrushabha.joshi@cygnetinfotech.com>
2020-12-05 14:01:03 +0530

    initial commit

---
+++ src/main/java/com/elasticsearch/synchronization/synchronization/SynchronizationApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.synchronization.synchronization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticsearch/synchronization/synchronization/SynchronizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.synchronization.synchronization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
